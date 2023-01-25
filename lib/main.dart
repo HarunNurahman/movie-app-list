78ece44b7e39f0d67205e24aae39c89a30de660a
Harun Nurahman <harun.nurrahman@hotmail.com>
2023-01-25 13:49:58 +0700

    Removing Unused Components

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// import 'package:device_preview/device_preview.dart';
-// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:movieapp_javan_devtest/pages/dashboard_page.dart';
 import 'package:movieapp_javan_devtest/pages/now-playing_page.dart';
@@ -7,13 +5,6 @@ import 'package:movieapp_javan_devtest/pages/top-rated_page.dart';
 import 'package:movieapp_javan_devtest/pages/upcoming_page.dart';
 
 void main() => runApp(const MyApp());
-
-// // Device Preview // //
-// void main() => runApp(DevicePreview(
-//       enabled: !kReleaseMode,
-//       builder: (context) => MyApp(),
-//     ));
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -27,9 +18,6 @@ class MyApp extends StatelessWidget {
         '/upcoming': (context) => const UpcomingMovie(),
       },
       debugShowCheckedModeBanner: false,
-      // // Device Preview // //
-      // locale: DevicePreview.locale(context),
-      // builder: DevicePreview.appBuilder,
     );
   }
 }
