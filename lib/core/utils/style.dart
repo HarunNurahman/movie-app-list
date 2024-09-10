b2ac6341f1721266faefa76a4610173a5f4c4b44
Harun Nurahman <harun.nurrahman@hotmail.com>
2024-09-10 11:11:13 +0700

    Refacture: change whole project structure (UI, style, etc.)

---
+++ lib/core/utils/style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Color palletes
+Color kprimaryColor = const Color(0xFF242A32);
+
+Color whiteColor = const Color(0xFFFFFFFF);
+Color grayColor = const Color(0xFF67686D);
+Color darkGrayColor = const Color(0xFF3A3F47);
+Color cyanColor = const Color(0xFF0296E5);
+Color orangeColor = const Color(0xFFFF8700);
+
+// Fonts
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+
+// Font weights
+FontWeight light = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-
-import 'pages/dashboard_page.dart';
-import 'pages/now-playing_page.dart';
-import 'pages/popular-movie_page.dart';
-
+import 'package:movie_app_list/presentation/pages/splash/splash_page.dart';
 
 void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        '/': (context) => const DashboardPage(),
-        '/now-playing': (context) => const NowPlaying(),
-        '/popular': (context) => const PopularMoviePage(),
-      },
       debugShowCheckedModeBanner: false,
+      home: SplashPage(),
     );
   }
 }
